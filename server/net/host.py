5e152582e8dbe575962d7d7ff8cf8b95f13d4ab5
chenfangwei <2462248263@qq.com>
2022-03-25 17:30:06 +0800

    #commit server

--- server/net/host.py
+++ server/net/host.py
@@ -1,41 +1,44 @@
+#!/usr/bin/env python
+
 import asyncio
+import json
+import secrets
+
 import websockets
 
-# 检测客户端权限，用户名密码通过才能退出循环
-async def check_permit(websocket):
-    while True:
-        recv_str = await websocket.recv()
-        cred_dict = recv_str.split(":")
-        if cred_dict[0] == "admin" and cred_dict[1] == "123456":
-            response_str = "congratulation, you have connect with server\r\nnow, you can do something else"
-            await websocket.send(response_str)
-            return True
+
+class Host(object):
+    def __init__(self):
+        self.ip = "127.0.0.1"
+        self.port = 3002
+
+    async def error(self, websocket, message):
+        event = {
+            "type": "error",
+            "message": message,
+        }
+        await websocket.send(json.dumps(event))
+
+    async def handler(self, websocket, path):
+        # Receive and parse the "init" event from the UI.
+        message = await websocket.recv()
+        event = json.loads(message)
+        assert event["type"] == "init"
+
+        if "join" in event:
+            # Second player joins an existing game.
+            pass
+        elif "watch" in event:
+            # Spectator watches an existing game.
+            pass
         else:
-            response_str = "sorry, the username or password is wrong, please submit again"
-            await websocket.send(response_str)
+            # First player starts a new game.
+            pass
 
-# 接收客户端消息并处理，这里只是简单把客户端发来的返回回去
-async def recv_msg(websocket):
-    while True:
-        recv_text = await websocket.recv()
-        response_text = f"your submit context: {recv_text}"
-        await websocket.send(response_text)
+    async def main(self):
+        async with websockets.serve(self.handler, self.ip, 3002):
+            await asyncio.Future()  # run forever
 
-# 服务器端主逻辑
-# websocket和path是该函数被回调时自动传过来的，不需要自己传
-async def main_logic(websocket, path):
-    await check_permit(websocket)
+    def run(self):
+        asyncio.run(self.main())
 
-    await recv_msg(websocket)
-
-# 把ip换成自己本地的ip
-start_server = websockets.serve(main_logic, '127.0.0.1', 5678)
-# 如果要给被回调的main_logic传递自定义参数，可使用以下形式
-# 一、修改回调形式
-# import functools
-# start_server = websockets.serve(functools.partial(main_logic, other_param="test_value"), '10.10.6.91', 5678)
-# 修改被回调函数定义，增加相应参数
-# async def main_logic(websocket, path, other_param)
-
-asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_forever()
